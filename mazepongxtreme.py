7393deac117c907b5a520e6cabad37559b08dde8
cyberllama <colin@kirby.net>
2014-05-01 16:17:43 +0000

    ponies r gr8

--- mazepongxtreme.py
+++ mazepongxtreme.py
@@ -1,10 +1,10 @@
 import sys
 
 name = raw_input ("What is your name? ")
-if (name=="Colin" or name== "Riley"):
+if (name=="Andrew" or name=="Clara" or name=="andrew" or name=="clara"):
     print ("Welcome superior being!")
-elif (name== "Andrew" or "Clara"):
-    print ("Welcome, thou saucy, raw-boned, clotpole. You human trash! Who the hooey fooey said you could be here?! See ya!")
+elif (name== "Colin" or name=="Riley" or name=="riley" or name=="colin"):
+    print ("Sorry... access denied. IQ level is insufficient")
     sys.exit()
 
 else:
@@ -24,14 +24,18 @@ from pygame.locals import*
 import math
 import random
 
+
+#Initiate PyGame
 pygame.init()
 width, height = 1000, 500
 screen= pygame.display.set_mode((width, height))
 
+#Declare Variables
 x = 500
 y = 250
-moveX = 3
-moveY = 3
+speed = 2
+moveX = 3*speed
+moveY = 3*speed
 keys = [False, False, False, False, False, False, False, False, False, False, False, False]
 counter = 0
 paddleY = 20
@@ -47,19 +51,22 @@ RPaddle = pygame.transform.scale(RPaddle, (40, 150))
 LPaddle = pygame.transform.scale(LPaddle, (40, 150))
 Ball = pygame.transform.scale(Ball, (50, 40))
 
+#Start game
 running = 1
 exitcode = 0
 while running:
     screen.fill(0x8F5498)
     screen.blit(Ball, (x,y))
-    screen.blit(RPaddle, (paddle1X,paddleY))
-    screen.blit(LPaddle, (paddleX,paddle1Y))
+    screen.blit(RPaddle, (paddle1X,paddle1Y))
+    screen.blit(LPaddle, (paddleX,paddleY))
     pygame.display.flip()
-    
+
+    #Closability
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             pygame.quit()
             exit(0)
+        #Keys
         if event.type == pygame.KEYDOWN:
             if event.key==K_w:
                 keys[0]=True
@@ -112,6 +119,15 @@ while running:
             elif event.key==K_y:
                 keys[11]=False
 
+        RPaddlerect=pygame.Rect(RPaddle.get_rect())
+        LPaddlerect=pygame.Rect(LPaddle.get_rect())
+        Ballrect=pygame.Rect(Ball.get_rect())
+        Ballrect.left
+        if Ballrect.colliderect(RPaddlerect):
+            moveX=-moveX
+        if Ballrect.colliderect(LPaddlerect):
+            moveX=-moveX
+
     if keys[8] and counter == 0:
         counter = 1
     if keys[9] and counter == 1:
@@ -125,11 +141,12 @@ while running:
     if keys[0:6] and counter < 5:
         counter = 0
 
+    #Creating boundaries
     if (paddleY >= 0 and paddleY <= 350):
         if keys[0]:
-            paddleY-= 4
+            paddleY-= 4*speed
         elif keys[2]:
-            paddleY+= 4
+            paddleY+= 4*speed
     else:
         if paddleY < 0:
             paddleY = 0
@@ -137,44 +154,60 @@ while running:
             paddleY = 350
     if (paddle1Y >= 0 and paddle1Y <= 350):
         if keys[1]:
-            paddle1Y-= 4
+            paddle1Y-= 4*speed
         elif keys[3]:
-            paddle1Y+= 4
+            paddle1Y+= 4*speed
     else:
         if paddle1Y < 0:
             paddle1Y = 0
         else:
             paddle1Y = 350
-    if (paddleX >= 0 and paddleX <= 960):
+
+
+
+
+            
+    """if (paddleX >= 0 and paddleX <= 960):
         if keys[4] and counter == 5:
             paddleX-= 4
         elif keys[6] and counter == 5:
             paddleX+= 4
     else:
         if paddleX < 0:
-            paddleX = 0
+            print("left player lost")
+            sys.exit()
         else:
-            paddleX = 960
-    if (paddle1X >= 0 and paddle1X <= 960):
+            print("right player lost")
+            sys.exit()
+
+    if(paddle1X >= 0 and paddle1X <= 960):
         if keys[5] and counter == 5:
             paddle1X-= 4
         elif keys[7] and counter == 5:
             paddle1X+= 4
     else:
+
+
         if paddle1X < 0:
             paddle1X = 0
         else:
-            paddle1X = 960
+            paddle1X = 960"""
 
     x += moveX
     y += moveY
 
-    if x < 0 or x > 950:
-        moveX = -moveX
+    if x < 0:
+        print("left player lost")
+        pygame.quit()
+        exit(0)
+    if x > 950:
+        print("right player lost")
+        pygame.quit()
+        exit(0)
     if y > 460 or y < 0:
         moveY = -moveY
 
-    if x < 50 or x > 900:
+    """if x < 50 or x > 900:
         if paddleY < y and (paddleY + 149) > y:
             moveX = -moveX
 
@@ -185,7 +218,9 @@ while running:
             moveX = -moveX
 
         elif paddle1Y < (y + 39) and (paddle1Y + 149) > (y + 39):
-            moveX = -moveX
+            moveX = -moveX"""
+
+    
 
 
 
